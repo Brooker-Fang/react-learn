bffd5b1a1628f9abd678cb8188bd87e9de786205
KingComedy <1455445725@qq.com>
2022-03-17 11:13:44 +0800

    note useRef

--- src/pages/hooks/useRefCom.js
+++ src/pages/hooks/useRefCom.js
@@ -1,29 +1,71 @@
 import React, { useRef, useEffect, useState } from 'react'
 /* 
   作用：
+    0 通过 useRef 来保证任何时候访问的 ref.current 都是最新的，以解决闭包问题。
+      延迟调用会存在闭包问题； 
+        1、使用 setTimeout、setInterval、Promise.then 等
+        2、useEffect 的卸载函数
     1 获取DOM元素对象
     2 用来保存 那些不会引起渲染的数据
 */
 export default () => {
-  const [count, setCount] = useState(0)
-  const userNameInput = useRef()
-  let timer = useRef()
+  const [count, setCount] = useState(0);
+  // 解决闭包问题
+  const countRef = useRef(count)
+  countRef.current = count;
+  // setTimeout 会造成闭包问题
   useEffect(() => {
-    // 保存于ref的current属性
-    console.log(userNameInput.current)
+    const timer = setTimeout(() => {
+      console.log('setTimeout count ===', count);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    }
   }, [])
+
+  // setInterval 会造成闭包问题
   useEffect(() => {
-    timer.current = setInterval(() => {
-      setCount(count => count + 1)
-    }, 1000)
+    const timer = setInterval(() => {
+      console.log('setInterval count ===', count);
+      console.log('setTimeout countRef ===', countRef.current);
+    }, 3000);
+    return () => {
+      clearInterval(timer);
+    }
   }, [])
-  const stopTimer = () => {
-    clearInterval(timer.current)
+
+  // Promise.then 会造成闭包问题
+  const getUsername = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('whh');
+      }, 3000);
+    })
   }
-  return <div>
-    <input ref={userNameInput}></input>
-    <div>{count}</div>
-    <button onClick={stopTimer}>清除定时</button>
-  </div>
+  useEffect(() => {
+    getUsername().then(() => {
+      console.log('Promise count ===', count);
+    });
+  }, [])
+
+  // useEffect 卸载函数会造成闭包问题
+  useEffect(() => {
+    return () => {
+      console.log('unmount count ===', count);
+    }
+  }, []);
+
+  return (
+    <>
+      Count: {count}
+      <button
+      onClick={() => setCount(c => c + 1)}
+    >
+      
+      click
+    </button>
+    </>
+    
+  )
   
 }
